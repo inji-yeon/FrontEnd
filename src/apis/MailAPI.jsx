26bef68bf2e80d5b4d039cad57362a7eaf98647f
Seopia <wjdwltjq7289@gmail.com>
2024-02-08 15:18:30 +0900

    login페이지 이동 가능

--- src/apis/MailAPI.jsx
+++ src/apis/MailAPI.jsx
@@ -115,7 +115,6 @@ export const fetchMailSearch = (word, option) =>{
                     } else {
                         dispatch(success(data));
                     }
-                    
                 }
                 
             })
